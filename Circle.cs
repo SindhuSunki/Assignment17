279f01875c901fe9861818df978c9eb0c8553b97
sindhusunki789@gmail.com <sindhusunki789@gmail.com>
2023-07-27 15:58:03 +0530

    first commit

---
+++ Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment17
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius) : base("Circle")
+        {
+            Radius = radius;
+        }
+
+        public override double Area => Math.PI * Radius * Radius;
+
+        public override double Perimeter => 2 * Math.PI * Radius;
+    }
+}
